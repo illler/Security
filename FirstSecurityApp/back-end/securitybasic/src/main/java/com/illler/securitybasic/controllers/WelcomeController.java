3e7a8f7ff4cc641931ec6c5d73b9aeac3472a30c
Alexandr Pyatunin <0250901@gmail.com>
2023-06-02 16:49:30 +0300

    made test controller

---
+++ FirstSecurityApp/back-end/securitybasic/src/main/java/com/illler/securitybasic/controllers/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.illler.securitybasic.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome";
+    }
+
+
+}
