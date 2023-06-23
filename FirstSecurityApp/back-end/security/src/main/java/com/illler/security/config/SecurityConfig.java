ebdd35aa0aab5dde7b80f8057c5162d511cf158c
Alexandr Pyatunin <0250901@gmail.com>
2023-06-23 19:22:38 +0300

    added in memory auth

--- FirstSecurityApp/back-end/security/src/main/java/com/illler/security/config/SecurityConfig.java
+++ FirstSecurityApp/back-end/security/src/main/java/com/illler/security/config/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,9 +19,24 @@ public class SecurityConfig {
                 .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                 .requestMatchers("/notices", "/contact").permitAll()
                 .and()
-                .formLogin();
+                .formLogin()
+                .and().httpBasic();
 
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .authorities("admin").build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .authorities("user").build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 
 }
